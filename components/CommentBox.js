ab9b9e59ee4df69bfa4303fbfd0d5d87ab4e7f91
sasata299 <karaage299@gmail.com>
2016-08-29 05:47:54 +0900

    サーバ側からデータを取得するように変更する

--- components/CommentBox.js
+++ components/CommentBox.js
@@ -1,17 +1,33 @@
 import React from 'react'
 import CommentList from './CommentList'
 import CommentFrom from './CommentFrom'
+import $ from 'jquery'
 
 export default class CommentBox extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: (data) => { this.setState({data: data}); },
+      error: (xhr, status, err) => {
+        console.error(this.props.url, status, err.toString());
+      }
+    });
   }
 
   render() {
     return(
       <div className='commentBox'>
         <h2>Comments</h2>
-        <CommentList data={this.props.data} />
+        <CommentList data={this.state.data} />
         <CommentFrom />
       </div>
     );
--- index.js
+++ index.js
@@ -2,12 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import CommentBox from './components/CommentBox'
 
-var data = [
-  {author: 'Pete Hunt', text: 'This is one comment'},
-  {author: 'Jordan Walke', text: 'This is *another* comment'}
-];
-
 ReactDOM.render(
-  <CommentBox data={data} />,
+  <CommentBox url="comments.json" />,
   document.getElementById('container')
 );
