d6a1d64daa561ec0aecf9353156965fcf62d2a4a
sasata299 <karaage299@gmail.com>
2016-09-11 08:08:45 +0900

    Comment, CommentListをstatelessコンポーネントにする

--- components/Comment.js
+++ components/Comment.js
@@ -1,16 +1,18 @@
-import Marked from 'marked'
 import React from 'react'
+import Marked from 'marked'
 
-export default class Commnet extends React.Component {
-  render() {
-    var rawMarkup = Marked(this.props.children.toString(), {sanitize: true});
-    return(
-      <div className='comment'>
-        <h3 className='commentAuthor'>
-          {this.props.author}
-        </h3>
-        <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
-      </div>
-    );
-  }
+const Comment = (props) => {
+  var rawMarkup = Marked(props.children.toString(), {sanitize: true});
+
+  return(
+    <div className='comment'>
+      <h3 className='commentAuthor'>
+        {props.author}
+      </h3>
+      <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
+    </div>
+  );
+
 }
+
+export default Comment;
--- components/CommentList.js
+++ components/CommentList.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import Comment from './Comment'
 
-export default class CommentList extends React.Component {
-  render() {
-    var commentNodes = this.props.data.map((comment)=> {
-      return(
-        <Comment author={comment.author}>{comment.text}</Comment>
-      );
-    });
+const CommentList = ({ data }) => {
+  var commentNodes = data.map((comment)=> {
     return(
-      <div className='commentList'>{commentNodes}</div>
+      <Comment key={`${comment.author}-${comment.text}`} author={comment.author}>{comment.text}</Comment>
     );
-  }
+  });
+
+  return(
+    <div className='commentList'>{commentNodes}</div>
+  );
 }
+
+export default CommentList;
